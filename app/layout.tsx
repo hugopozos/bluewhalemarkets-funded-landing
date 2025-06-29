a3f4cd7648116c476b903d0d188a4e18832f55c8
Henry Lucena <henrylucena7@gmail.com>
2025-06-29 11:51:23 -0500

    Eliminado SmoothScroll y agregado Pixel de Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ const manrope = Manrope({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Blue Whale Markets Funded",
   description: "Trade with our capital and keep up to 90% of the profits. Simple, transparent, and fair.",
-  generator: 'Next.js',
+  generator: "Next.js",
   icons: {
-    icon: '/images/logo-favicon.webp',
-    apple: '/images/logo-favicon.webp',
-    shortcut: '/images/logo-favicon.webp'
+    icon: "/images/logo-favicon.webp",
+    apple: "/images/logo-favicon.webp",
+    shortcut: "/images/logo-favicon.webp"
   }
 }
 
@@ -26,9 +26,33 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/logo-favicon.webp" />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '410161175181122'); 
+              fbq('track', 'PageView');
+            `
+          }}
+        />
+
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=410161175181122&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={manrope.className}>
-        <SmoothScroll>{children}</SmoothScroll>
+        {children}
       </body>
     </html>
   )
