e138a8b8aa527df21104e063bf3f12d75ed28c6a
Henry Lucena <henrylucena7@gmail.com>
2025-07-04 17:59:52 -0500

    solución enlazado del menú header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Manrope } from "next/font/google"
 import "./globals.css"
+import SmoothScroll from "@/components/smooth-scroll";
 
 const manrope = Manrope({ subsets: ["latin"] })
 
